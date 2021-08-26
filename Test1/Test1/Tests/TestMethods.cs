34f4f57fa819536a296d251e6f3be74880c36926
YevgeniyYakuta <yakutawork@gmail.com>
2021-08-26 17:26:58 +0300

    ssssssssssss

---
+++ Test1/Test1/Tests/TestMethods.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Test1.Models;
+
+namespace Test1.Tests
+{
+    class TestMethods
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("https://vk.com/", InfoResponse.Url, "is not equal");
+
+            Assert.AreEqual("https://api.vk.com/method", InfoResponse.ApiUrl, "Name is wrong");
+
+            Assert.AreEqual("PuV6j_.2&$m9h?UYY", User.Password, "password is wrong");
+
+            Assert.AreEqual("+375291660762", User.Login, "login is wrong");
+        }
+    }
+}
--- Test1/Test1/UnitTest1.cs
+++ Test1/Test1/UnitTest1.cs
@@ -3,24 +3,18 @@ using Test1.Models;
 
 namespace Test1
 {
-    public class Tests
-    { 
+    //public class Tests
+    //{ 
+    //    [Test]
+    //    public void Test1()
+    //    {
+    //        Assert.AreEqual("https://vk.com/", InfoResponse.Url, "is not equal");
 
-        [SetUp]
-        public void Setup()
-        {  
-        }
+    //        Assert.AreEqual("https://api.vk.com/method", InfoResponse.ApiUrl, "Name is wrong");
 
-        [Test]
-        public void Test1()
-        {
-            Assert.AreEqual("https://vk.com/", InfoResponse.Url, "is not equal");
+    //        Assert.AreEqual("PuV6j_.2&$m9h?UYY", User.Password, "password is wrong");
 
-            Assert.AreEqual("https://api.vk.com/method", InfoResponse.ApiUrl, "Name is wrong");
-
-            Assert.AreEqual("PuV6j_.2&$m9h?UYY", User.Password, "password is wrong");
-
-            Assert.AreEqual("+375291660762", User.Login, "login is wrong");
-        }
-    }
+    //        Assert.AreEqual("+375291660762", User.Login, "login is wrong");
+    //    }
+    //}
 }
