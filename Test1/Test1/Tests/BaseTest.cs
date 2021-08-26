e0698bb25c8d5ddee522e4ba681da1e90a774bee
YevgeniyYakuta <yakutawork@gmail.com>
2021-08-26 17:23:30 +0300

    dddddddddddd

---
+++ Test1/Test1/Tests/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Aquality.Selenium.Browsers;
+namespace Test1.Tests
+{
+   public class BaseTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            AqualityServices.Browser.GoTo(InfoResponse.Url);
+            AqualityServices.Browser.Maximize();
+            AqualityServices.Browser.WaitForPageToLoad();
+        }
+    }
+}
