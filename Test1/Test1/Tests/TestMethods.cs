61f6910924aaa5766a103f92fe48e93e8481deac
YevgeniyYakuta <yakutawork@gmail.com>
2021-08-26 17:43:06 +0300

    test2

--- Test1/Test1/Tests/TestMethods.cs
+++ Test1/Test1/Tests/TestMethods.cs
@@ -8,24 +8,14 @@ namespace Test1.Tests
 {
     [TestFixture]
     class TestMethods
-    {
-        [SetUp]
-        public void SetUp()
+    {         
+        [Test]
+        public void Test1()
         {
             AqualityServices.Browser.GoTo(InfoResponse.Url);
             AqualityServices.Browser.Maximize();
             AqualityServices.Browser.WaitForPageToLoad();
-        }
 
-        [TearDown]
-        public void TearDown()
-        {
-            AqualityServices.Browser.Quit();
-        }
-
-        [Test]
-        public void Test1()
-        {
             Assert.AreEqual("https://vk.com/", InfoResponse.Url, "is not equal");
 
             Assert.AreEqual("https://api.vk.com/method", InfoResponse.ApiUrl, "Name is wrong");
@@ -33,6 +23,7 @@ namespace Test1.Tests
             Assert.AreEqual("PuV6j_.2&$m9h?UYY", User.Password, "password is wrong");
 
             Assert.AreEqual("+375291660762", User.Login, "login is wrong");
+            AqualityServices.Browser.Quit();
         }
     }
 }
