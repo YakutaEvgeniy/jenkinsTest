4bcd35c201762639fb4572f1262901cd06458557
YevgeniyYakuta <yakutawork@gmail.com>
2021-08-26 16:09:36 +0300

    ssssssss

--- Test1/Test1/Models/User.cs
+++ Test1/Test1/Models/User.cs
@@ -9,7 +9,7 @@ namespace Test1.Models
 {
     public static class User
     {
-        public static int Password => GetTestDataFromFile().GetValue<int>(".password");
+        public static string Password => GetTestDataFromFile().GetValue<string>(".password");
 
         public static string Login => GetTestDataFromFile().GetValue<string>(".login");
 
